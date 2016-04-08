47a28d233579942e381ce02505af1f7bff08d1fb
Michael Sarahan <msarahan@gmail.com>
2016-04-08 07:42:44 -0500

    sync up with Continuum recipe.  update to 15.2.

--- recipes/pyzmq/build.sh
+++ recipes/pyzmq/build.sh
@@ -5,3 +5,9 @@ export DYLD_LIBRARY_PATH="$PREFIX/lib:$DYLD_LIBRARY_PATH"
 "$PYTHON" setup.py configure --zmq "$PREFIX"
 
 "$PYTHON" setup.py install
+
+if [ $ANA_PY == 27 ]; then
+    rm $SP_DIR/zmq/asyncio.py
+    rm $SP_DIR/zmq/auth/asyncio.py
+    rm $SP_DIR/zmq/tests/_test_asyncio.py
+fi
